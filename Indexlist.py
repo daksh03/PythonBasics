de81c8004e94e937e5f764f5eac8cb941e46ac63
daksh03 <dakshchoudhary.1234@gmail.com>
2026-05-29 09:41:41 +0530

    Day 3

---
+++ Indexlist.py
@@ -0,0 +1,10 @@
+lst=[[0, 2], [4, 6], [8, 10], [12, 14]]
+print(lst[0])
+print(lst[-1][1])
+lst2=["chair", "table", "desk", "lamp", "bed"]
+print(lst2[-5])
+print("Most people own at least "+ str(lst[0][1])+" "+ lst2[0]+".")
+lst3=[0.98, 8.76, 6.54, 4.32]
+print(lst3[1:])
+print(lst3[1:3])
+print(lst3[0:2])
---
+++ Lists.py
@@ -0,0 +1,6 @@
+lst=[8,True,"String",[1,2,3]]
+print(lst)
+lst1=list("hello")
+print(lst1)
+print("e" in lst1)
+print("e" not in lst1)
